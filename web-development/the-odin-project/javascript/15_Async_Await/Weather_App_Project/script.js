f6eb15ad8f0ad951a8f62d2fc799f47dd9c2cf96
Andrew Nguyen <anguyen0615@gmail.com>
2026-09-23 09:51:52 -0500

    Weather app project

--- web-development/the-odin-project/javascript/15_Async_Await/Weather_App_Project/script.js
+++ web-development/the-odin-project/javascript/15_Async_Await/Weather_App_Project/script.js
@@ -1,16 +1,33 @@
 
+
 const getWeather = async () => {
     const location = document.getElementById('location').value;
-
     const date1 = document.getElementById('date1').value;
     const date2 = document.getElementById('date2').value;
+    const temp = document.getElementById('temp');
+    const tempLabel = document.getElementById('tempLabel');
 
-    console.log(location);
-    console.log(date1);
-    console.log(date2);
-
-    const resp = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/?key=YOUR_API_KEY`)
+    const resp = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/?key=`)
     const data = await resp.json();
 
-    console.log(data);
+    temp.innerText = data?.currentConditions?.temp;
+
+    if (data.currentConditions.temp) {
+        tempLabel.innerText = 'Fahrenheit'
+    }
+}
+
+const toggleTemp = () => {
+    const temp = document.getElementById('temp');
+    const tempLabel = document.getElementById('tempLabel');
+
+    if (temp.innerText && tempLabel.innerText === 'Celsius') {
+        const fah = (1.8 * Number(temp.innerText)) + 32;
+        temp.innerText = fah.toFixed(1);
+        tempLabel.innerText = 'Fahrenheit'
+    } else {
+        const cel = (Number(temp.innerText) - 32) * (0.55555556);
+        temp.innerText = cel.toFixed(1);
+        tempLabel.innerText = 'Celsius'
+    }
 }
